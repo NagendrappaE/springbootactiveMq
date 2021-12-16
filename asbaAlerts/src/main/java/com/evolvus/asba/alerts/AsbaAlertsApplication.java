5882d337e99a0c8e9f6f87b5d9325a285533a7f1
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2021-12-16 18:42:11 +0530

    active mq

---
+++ asbaAlerts/src/main/java/com/evolvus/asba/alerts/AsbaAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.evolvus.asba.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsbaAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsbaAlertsApplication.class, args);
+	}
+
+}
---
+++ asbaAlerts/src/test/java/com/evolvus/asba/alerts/AsbaAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolvus.asba.alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsbaAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
